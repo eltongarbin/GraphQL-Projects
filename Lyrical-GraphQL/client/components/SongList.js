b09ed37af3050a2ddf3cfdea5c81122a094d13bb
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-02-02 21:04:04 -0200

    Clientside GraphQL

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        SongList
+      </div>
+    );
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
