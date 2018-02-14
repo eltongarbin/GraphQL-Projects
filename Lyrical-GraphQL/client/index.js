006056c0cc9fd5a602f835d64b0f8cd915536297
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-02-14 21:40:03 -0200

    Implementing graphql in SongCreate component and routing

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({
   link: new HttpLink(),
@@ -15,12 +17,14 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <HashRouter>
+        <div className="container">
+          <Route exact path="/" component={SongList} />
+          <Route path="/songs/new" component={SongCreate} />
+        </div>
+      </HashRouter>
     </ApolloProvider>
   );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
