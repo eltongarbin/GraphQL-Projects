7d21774f2d1aabcfcdd5ab1ee779a280feab503f
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-03-20 20:37:50 -0300

    Automatic Component Rerenders

--- auth-graphql/client/components/Header.js
+++ auth-graphql/client/components/Header.js
@@ -3,15 +3,22 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router-dom';
 
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
     if (loading) return <div />;
     if (user) {
       return (
         <li>
-          <Link to="/logout">Logout</Link>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
         </li>
       );
     } else {
@@ -42,4 +49,4 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
