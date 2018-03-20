a066d3addc19f3694d3a80f0b14f5d0a05ad2428
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-03-20 20:22:37 -0300

    Login and Logout Buttons

--- auth-graphql/client/components/Header.js
+++ auth-graphql/client/components/Header.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+    if (loading) return <div />;
+    if (user) {
+      return (
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+      );
+    } else {
+      return (
+        <Fragment>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </Fragment>
+      );
+    }
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
